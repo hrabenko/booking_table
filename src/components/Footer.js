20539dbe02ab586cc1b5b5b62947c1d80901b2a2
Kristina Hrabenko <kgrabenko03@gmail.com>
2023-10-13 20:56:18 +0300

    created components

---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+function Footer () {
+    return(
+        <footer>
+            <div>
+                <img src="../images/footer_logo.png" alt="Logo" />
+            </div>
+            <div>
+                <h3>Navigation</h3>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Reservations</a></li>
+                    <li><a href="#">Order Online</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>Contact</h3>
+                <ul>
+                    <li>123 Main Street, Smalltown, USA</li>
+                    <li><a href="tel:+11111111111">+1 111 111 11 11</a></li>
+                    <li><a href="mailto:littlelemon@gmail.com">littlelemon@gmail.com</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>Social Media</h3>
+                <ul>
+                    <li><a>Instagram</a></li>
+                    <li><a>Facebook</a></li>
+                </ul>
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+function Header () {
+    return(
+        <header>
+            <img src="../images/header_logo.png" alt="Logo" />
+            <Nav />
+        </header>
+    )
+}
+
+export default Header;
