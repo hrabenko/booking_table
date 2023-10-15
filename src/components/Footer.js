533175f5075fecf25db68afef0fddbde5d9dbed3
Kristina Hrabenko <kgrabenko03@gmail.com>
2023-10-15 15:59:33 +0300

    styled header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,39 @@
+import "./Footer.css";
 function Footer () {
     return(
         <footer>
             <div>
-                <img src="/images/footer_logo.png" alt="Logo" />
+                <img className="footer_logo" src="/images/footer_logo.png" alt="Logo" />
             </div>
-            <div>
-                <h3>Navigation</h3>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">About</a></li>
-                    <li><a href="/">Menu</a></li>
-                    <li><a href="/">Reservations</a></li>
-                    <li><a href="/">Order Online</a></li>
-                    <li><a href="/">Login</a></li>
-                </ul>
+            <div className="list-containers">
+                <div>
+                    <h3>Navigation</h3>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Menu</a></li>
+                        <li><a href="/">Reservations</a></li>
+                        <li><a href="/">Order Online</a></li>
+                        <li><a href="/">Login</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Contact</h3>
+                    <ul>
+                        <li>123 Main Street, Smalltown, USA</li>
+                        <li><a href="tel:+11111111111">+1 111 111 11 11</a></li>
+                        <li><a href="mailto:littlelemon@gmail.com">littlelemon@gmail.com</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Social Media</h3>
+                    <ul>
+                        <li><a href="https://www.instagram.com/">Instagram</a></li>
+                        <li><a href="https://www.facebook.com/">Facebook</a></li>
+                    </ul>
+                </div>
             </div>
-            <div>
-                <h3>Contact</h3>
-                <ul>
-                    <li>123 Main Street, Smalltown, USA</li>
-                    <li><a href="tel:+11111111111">+1 111 111 11 11</a></li>
-                    <li><a href="mailto:littlelemon@gmail.com">littlelemon@gmail.com</a></li>
-                </ul>
-            </div>
-            <div>
-                <h3>Social Media</h3>
-                <ul>
-                    <li><a href="https://www.instagram.com/">Instagram</a></li>
-                    <li><a href="https://www.facebook.com/">Facebook</a></li>
-                </ul>
-            </div>
+
 
         </footer>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import Nav from "./Nav";
+import './Header.css';
+
 
 function Header () {
     return(
         <header>
-            <img src="images/header_logo.png" alt="Logo" />
+            <img className="header-logo" src="images/header_logo.png" alt="Logo" />
             <Nav />
         </header>
     )
